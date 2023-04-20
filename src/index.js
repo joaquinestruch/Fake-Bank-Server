0f884dfbd6fc7d7a465ecfe0ade8daf4152cfdfa
joaquin <joacoetruuu@gmail.com>
2023-04-20 19:59:44 -0300

    cors update

--- src/index.js
+++ src/index.js
@@ -22,6 +22,12 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 app.use(express.json());
 app.use("/api", userRoutes); 
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 
 //routes
 app.get("/", (req, res) => {
